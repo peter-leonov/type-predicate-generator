be1c5eeb656890ec78676c3e09a6f571e7531ab3
Peter Leonov <gojpeg@gmail.com>
2025-01-03 22:23:10 +0100

    actually test the guards

--- tests/integration/guards.test.ts
+++ tests/integration/guards.test.ts
@@ -1,7 +1,264 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
 
-import { isUser } from "./guards";
+import { isPost, isUser } from "./guards";
 
-test("valid User", () => {
-  expect(isUser({})).toBe(true);
+const validUser = [
+  {
+    id: 1,
+    login: "string",
+    bio: {
+      first: "string",
+      last: "string",
+    },
+  },
+  {
+    _extra: 1,
+    id: 1,
+    login: "string",
+    bio: {
+      _extra: 1,
+      first: "string",
+      last: "string",
+    },
+  },
+];
+
+const invalidUser = [
+  null,
+  {},
+  {
+    login: "string",
+    bio: {
+      first: "string",
+      last: "string",
+    },
+  },
+  {
+    id: 1,
+    bio: {
+      first: "string",
+      last: "string",
+    },
+  },
+  {
+    id: 1,
+    login: "string",
+  },
+  {
+    id: 1,
+    login: "string",
+    bio: null,
+  },
+  {
+    id: 1,
+    login: "string",
+    bio: {},
+  },
+  {
+    id: 1,
+    login: "string",
+    bio: {
+      last: "string",
+    },
+  },
+  {
+    id: 1,
+    login: "string",
+    bio: {
+      first: "string",
+    },
+  },
+
+  {
+    id: "string",
+    login: "string",
+    bio: {
+      first: "string",
+      last: "string",
+    },
+  },
+  {
+    id: 1,
+    login: 1,
+    bio: {
+      first: "string",
+      last: "string",
+    },
+  },
+  {
+    id: 1,
+    login: "string",
+    bio: 1,
+  },
+  {
+    id: 1,
+    login: "string",
+    bio: {
+      first: 1,
+      last: "string",
+    },
+  },
+  {
+    id: 1,
+    login: "string",
+    bio: {
+      first: "string",
+      last: 1,
+    },
+  },
+];
+
+describe(isUser, () => {
+  it.for(validUser)("valid", (value) => {
+    expect(isUser(value)).toBe(true);
+  });
+  it.for(invalidUser)("invalid", (value) => {
+    expect(isUser(value)).toBe(false);
+  });
+});
+
+const validPost = [
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: false,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    published: true,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: ["string"],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: ["string", 1],
+  },
+];
+
+const invalidPost = [
+  null,
+  {
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+  },
+  {
+    title: 1,
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: 1,
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: 1,
+    published: true,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: 1,
+    author: validUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    author: invalidUser[0],
+    list: [1],
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: null,
+  },
+  {
+    title: "string",
+    text: "string",
+    link: "string",
+    published: true,
+    author: validUser[0],
+    list: [true],
+  },
+];
+
+describe(isPost, () => {
+  it.for(validPost)("valid", (value) => {
+    expect(isPost(value)).toBe(true);
+  });
+  it.for(invalidPost)("invalid", (value) => {
+    expect(isPost(value)).toBe(false);
+  });
 });
