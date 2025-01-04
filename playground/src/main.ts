b5718a9d2dd058a2ab5985af81b5728b0e5187a8
Peter Leonov <gojpeg@gmail.com>
2025-01-04 15:14:25 +0100

    layout and the editor

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,1 +1,47 @@
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import "./style.css";
+import "./worker";
+
+function $(selector: string) {
+  return document.querySelector<HTMLElement>(selector);
+}
+
+export function ok(value: unknown): asserts value {
+  if (!value) {
+    throw new Error(`${value} is not truthy`);
+  }
+}
+
+const sourceRoot = $("#source");
+ok(sourceRoot);
+
+const example = `export type User = {
+  id: number;
+  login: string;
+  bio: {
+    first: string;
+    last: string;
+  };
+};
+
+export type Post = {
+  title: string;
+  text: string;
+  link?: string;
+  published: boolean;
+  author: User;
+  list: Array<number | string>;
+};
+`;
+
+const editor = monaco.editor.create(sourceRoot, {
+  theme: "vs-dark",
+  minimap: { enabled: false },
+  value: example,
+  language: "typescript",
+});
+
+editor.onDidChangeModelContent(() => {
+  console.log("!!!!!!!!!!!!HERE!!!!!!!!!!!!!!!");
+  console.log(editor.getValue());
+});
