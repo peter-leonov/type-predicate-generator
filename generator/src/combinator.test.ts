523f3a13982477002c7cda8acebf28d0e8f71af5
Peter Leonov <gojpeg@gmail.com>
2025-01-07 12:15:41 +0100

    test nested objects

--- generator/src/combinator.test.ts
+++ generator/src/combinator.test.ts
@@ -88,4 +88,72 @@ describe("object", () => {
       combine(object({ a: value("A"), b: value("B"), c: value("C") }))
     ).toMatchSnapshot();
   });
+
+  it("of a single union property", () => {
+    expect(
+      combine(object({ a: union([value("A1"), value("A2")]) }))
+    ).toMatchSnapshot();
+  });
+
+  it("of a several union properties", () => {
+    expect(
+      combine(
+        object({
+          a: union([value("A1")]),
+          b: union([value("B1"), value("B2")]),
+          c: union([value("C1"), value("C2"), value("C3")]),
+        })
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("of a several objects with value properties", () => {
+    expect(
+      combine(
+        object({
+          a: object({ a1: value("A1"), a2: value("A2") }),
+          b: object({ b1: value("B1"), b2: value("B2") }),
+          c: object({ c1: value("C1"), c2: value("C2") }),
+        })
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("of a several objects with union properties", () => {
+    expect(
+      combine(
+        object({
+          a: object({ aa: union([value("AA1"), value("AA2")]) }),
+          b: object({ bb: union([value("BB1"), value("BB2")]) }),
+          c: object({ cc: union([value("CC1"), value("CC2")]) }),
+        })
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("of a several nested objects with a value property", () => {
+    expect(
+      combine(
+        object({
+          a: object({
+            b: object({ c: object({ d: value("D") }) }),
+          }),
+        })
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("of a several nested objects with a union property", () => {
+    expect(
+      combine(
+        object({
+          a: object({
+            b: object({
+              c: object({ d: union([value("D1"), value("D2")]) }),
+            }),
+          }),
+        })
+      )
+    ).toMatchSnapshot();
+  });
 });
