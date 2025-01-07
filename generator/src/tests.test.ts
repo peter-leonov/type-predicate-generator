d920a35716cff8e5299e5ee8bc315912bfff9460
Peter Leonov <gojpeg@gmail.com>
2025-01-07 11:56:35 +0100

    some more tests

--- generator/src/tests.test.ts
+++ generator/src/tests.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
 import {
   array,
   combine,
@@ -8,7 +8,7 @@ import {
   type Value,
 } from "./tests";
 
-test(combine, () => {
+it("all at once", () => {
   const obj = object<Value>({
     a: union([value(1), value(2)]),
     b: union([value(true), value(false)]),
@@ -19,4 +19,45 @@ test(combine, () => {
   });
 
   expect(combine(obj)).toMatchSnapshot();
+});
+
+describe("value", () => {
+  it("number", () => {
+    expect(combine(value(1))).toMatchSnapshot();
+  });
+  it("string", () => {
+    expect(combine(value("a"))).toMatchSnapshot();
+  });
+  it("null", () => {
+    expect(combine(value(null))).toMatchSnapshot();
+  });
+});
+
+describe("union", () => {
+  it("of a single value", () => {
+    expect(combine(union([value(1)]))).toMatchSnapshot();
+  });
+
+  it("of several values", () => {
+    expect(
+      combine(union([value(1), value(2), value(3)]))
+    ).toMatchSnapshot();
+  });
+
+  it("of a union", () => {
+    expect(
+      combine(union([union([value(1), value(2), value(3)])]))
+    ).toMatchSnapshot();
+  });
+
+  it("of unions", () => {
+    expect(
+      combine(
+        union([
+          union([value(1), value(2)]),
+          union([value(3), value(4)]),
+        ])
+      )
+    ).toMatchSnapshot();
+  });
 });
