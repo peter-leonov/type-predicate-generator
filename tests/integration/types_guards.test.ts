f1e6eb1ba655923fc839e36227a09c9a07e4d116
Peter Leonov <gojpeg@gmail.com>
2025-01-10 18:23:42 +0100

    fix CI

--- tests/integration/types_guards.test.ts
+++ tests/integration/types_guards.test.ts
@@ -2,35 +2,39 @@ import { expect, describe, it } from "vitest";
 import { isUser } from "./types_guards";
 const invalidValue: any = Symbol("invalidValue");
 const valid_User = [
-    { "id": 0, "login": "string", "bio": { "first": "string", "last": "string" } },
-    { "id": 42, "login": "string", "bio": { "first": "string", "last": "string" } }
+  { id: 0, login: "string", bio: { first: "string", last: "string" } },
+  { id: 42, login: "string", bio: { first: "string", last: "string" } },
 ];
 const invalid_User = [
-    invalidValue,
-    { "login": "string", "bio": { "first": "string", "last": "string" } },
-    { "id": invalidValue, "login": "string", "bio": { "first": "string", "last": "string" } },
-    { "id": 0, "bio": { "first": "string", "last": "string" } },
-    { "id": 0, "login": invalidValue, "bio": { "first": "string", "last": "string" } },
-    { "id": 0, "login": "string" },
-    { "id": 0, "login": "string", "bio": invalidValue },
-    { "id": 0, "login": "string", "bio": { "last": "string" } },
-    { "id": 0, "login": "string", "bio": { "first": invalidValue, "last": "string" } },
-    { "id": 0, "login": "string", "bio": { "first": "string" } },
-    { "id": 0, "login": "string", "bio": { "first": "string", "last": invalidValue } },
-    { "id": 42, "bio": { "first": "string", "last": "string" } },
-    { "id": 42, "login": invalidValue, "bio": { "first": "string", "last": "string" } },
-    { "id": 42, "login": "string" },
-    { "id": 42, "login": "string", "bio": invalidValue },
-    { "id": 42, "login": "string", "bio": { "last": "string" } },
-    { "id": 42, "login": "string", "bio": { "first": invalidValue, "last": "string" } },
-    { "id": 42, "login": "string", "bio": { "first": "string" } },
-    { "id": 42, "login": "string", "bio": { "first": "string", "last": invalidValue } }
+  invalidValue,
+  { login: "string", bio: { first: "string", last: "string" } },
+  {
+    id: invalidValue,
+    login: "string",
+    bio: { first: "string", last: "string" },
+  },
+  { id: 0, bio: { first: "string", last: "string" } },
+  { id: 0, login: invalidValue, bio: { first: "string", last: "string" } },
+  { id: 0, login: "string" },
+  { id: 0, login: "string", bio: invalidValue },
+  { id: 0, login: "string", bio: { last: "string" } },
+  { id: 0, login: "string", bio: { first: invalidValue, last: "string" } },
+  { id: 0, login: "string", bio: { first: "string" } },
+  { id: 0, login: "string", bio: { first: "string", last: invalidValue } },
+  { id: 42, bio: { first: "string", last: "string" } },
+  { id: 42, login: invalidValue, bio: { first: "string", last: "string" } },
+  { id: 42, login: "string" },
+  { id: 42, login: "string", bio: invalidValue },
+  { id: 42, login: "string", bio: { last: "string" } },
+  { id: 42, login: "string", bio: { first: invalidValue, last: "string" } },
+  { id: 42, login: "string", bio: { first: "string" } },
+  { id: 42, login: "string", bio: { first: "string", last: invalidValue } },
 ];
 describe("User", () => {
-    it.for(valid_User)("valid", value => {
-        expect(isUser(value)).toBe(true);
-    });
-    it.for(invalid_User)("invalid", value => {
-        expect(isUser(value)).toBe(false);
-    });
+  it.for(valid_User)("valid", (value) => {
+    expect(isUser(value)).toBe(true);
+  });
+  it.for(invalid_User)("invalid", (value) => {
+    expect(isUser(value)).toBe(false);
+  });
 });
--- tests/integration/types_guards.ts
+++ tests/integration/types_guards.ts
@@ -1,38 +1,38 @@
 import { type User } from "./types";
 type SafeShallowShape<Type> = {
-    [_ in keyof Type]?: unknown;
+  [_ in keyof Type]?: unknown;
 };
 const safeIsArray: (v: unknown) => v is unknown[] = Array.isArray;
 export function isUser(root: unknown): root is User {
-    if (!(typeof root === "object" && root !== null)) {
-        return false;
-    }
-    (root) satisfies {};
-    const { id, login, bio }: SafeShallowShape<User> = root;
-    if (!(typeof id === "number")) {
-        return false;
-    }
-    if (!(typeof login === "string")) {
-        return false;
-    }
-    if (!(typeof bio === "object" && bio !== null)) {
-        return false;
-    }
-    (bio) satisfies {};
-    const { first, last }: SafeShallowShape<User["bio"]> = bio;
-    if (!(typeof first === "string")) {
-        return false;
-    }
-    if (!(typeof last === "string")) {
-        return false;
-    }
-    ({
-        id,
-        login,
-        bio: {
-            first,
-            last
-        }
-    }) satisfies User;
-    return true;
+  if (!(typeof root === "object" && root !== null)) {
+    return false;
+  }
+  root satisfies {};
+  const { id, login, bio }: SafeShallowShape<User> = root;
+  if (!(typeof id === "number")) {
+    return false;
+  }
+  if (!(typeof login === "string")) {
+    return false;
+  }
+  if (!(typeof bio === "object" && bio !== null)) {
+    return false;
+  }
+  bio satisfies {};
+  const { first, last }: SafeShallowShape<User["bio"]> = bio;
+  if (!(typeof first === "string")) {
+    return false;
+  }
+  if (!(typeof last === "string")) {
+    return false;
+  }
+  ({
+    id,
+    login,
+    bio: {
+      first,
+      last,
+    },
+  }) satisfies User;
+  return true;
 }
