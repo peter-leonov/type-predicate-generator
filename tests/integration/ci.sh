829afc8948dda6a8a57385aea634133be75fa618
Peter Leonov <gojpeg@gmail.com>
2025-01-11 22:40:37 +0100

    minify using esbuild

--- tests/integration/ci.sh
+++ tests/integration/ci.sh
@@ -12,6 +12,8 @@ npm i
 rm -rf types_guards.ts types_guards.test.ts
 npx type-predicate-generator --unitTests "./types.ts"
 
+npx esbuild --bundle --outfile=types_guards.min.js --minify "types_guards.ts"
+
 rm unsupported.1.txt unsupported.2.txt
 npx type-predicate-generator ./unsupported.ts 1>unsupported.1.txt 2>unsupported.2.txt || true
 
