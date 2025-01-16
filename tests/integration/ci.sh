f50ee46b5476ce03d7e0ffa85acca2714d9d0efc
Peter Leonov <gojpeg@gmail.com>
2025-01-16 21:51:05 +0100

    rename

--- tests/integration/ci.sh
+++ tests/integration/ci.sh
@@ -19,8 +19,8 @@ rm -rf example_guards.ts example_guards.test.ts
 npx type-predicate-generator --unitTests src/example.ts
 npx esbuild --bundle --outfile=src/example_guards.min.js --minify "src/example_guards.ts"
 
-rm src/unsupported.1.txt src/unsupported.2.txt
-npx type-predicate-generator ./src/unsupported.ts 1>src/unsupported.1.txt 2>src/unsupported.2.txt || true
+rm src/unsupported1.stdout.txt src/unsupported1.stderr.txt
+npx type-predicate-generator ./src/unsupported1.ts 1>src/unsupported1.stdout.txt 2>src/unsupported1.stderr.txt || true
 
 npm run typecheck
 CI=true npm test
