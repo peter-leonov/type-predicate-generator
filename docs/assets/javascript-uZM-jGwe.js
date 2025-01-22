7cdc553f669f8848b7face355834fa20e108afd3
Peter Leonov <gojpeg@gmail.com>
2025-01-22 10:17:25 +0100

    update playground

--- docs/assets/javascript-uZM-jGwe.js
+++ docs/assets/javascript-uZM-jGwe.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-DD1C3V5s.js";import"./index-CPVKG8nz.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-CZmaY1HZ.js";import"./index-5v8WT6XG.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
