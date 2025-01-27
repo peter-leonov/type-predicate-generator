1ca938d1d38e93c312ee35dece12241e0460153e
Peter Leonov <gojpeg@gmail.com>
2025-01-28 00:04:25 +0100

    control output

--- tests/integration/tests.sh
+++ tests/integration/tests.sh
@@ -5,7 +5,7 @@ set -eux -o pipefail
 for test in src/tests/*; do
   echo "running $test"
   rm -rf "${test}/types_guards.ts"
-  npx type-predicate-generator "$@" "${test}/types.ts"
+  npx type-predicate-generator "$@" "${test}/types.ts" 1>"${test}/stdout.txt" 2>"${test}/stderr.txt"
   npx esbuild --bundle --outfile="${test}/types_guards.min.js" --minify "${test}/types_guards.ts"
   npx prettier -w "${test}"
 done
