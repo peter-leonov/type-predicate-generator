7d625c14e12994073cd07563c1391ae27f64cc78
Peter Leonov <gojpeg@gmail.com>
2025-01-28 00:13:49 +0100

    merge back

--- tests/integration/ci.sh
+++ tests/integration/ci.sh
@@ -11,13 +11,23 @@ npm i --no-audit
 
 npx type-predicate-generator --help
 
-./tests.sh
-# to update unit tests that might require manual fixing:
-# ./tests.sh --unitTests
+for test in src/tests/*; do
+  echo "running $test"
+  rm -rf "${test}/types_guards.ts" "${test}/types_guards.min.js" "${test}/status.ts" "${test}/stdout.txt" "${test}/stderr.txt"
+
+  # to update unit tests that might require manual fixing add --unitTests
+  if npx type-predicate-generator "${test}/types.ts" 1>"${test}/stdout.txt" 2>"${test}/stderr.txt"; then
+    echo "$?" > "${test}/status.ts"
+  else
+    echo "$?" > "${test}/status.ts"
+  fi
+  
+  if test -f "${test}/types_guards.ts"; then
+  npx esbuild --bundle --outfile="${test}/types_guards.min.js" --minify "${test}/types_guards.ts"
+  fi
+done
 
 npm run typecheck
 CI=true npm test
-
 npx prettier -w .
-
 git diff --exit-code
