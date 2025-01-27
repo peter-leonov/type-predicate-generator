56489a9d0cacd52597e0755674d1d1a216b8d0c4
Peter Leonov <gojpeg@gmail.com>
2025-01-28 00:04:13 +0100

    move more tests

--- tests/integration/ci.sh
+++ tests/integration/ci.sh
@@ -11,14 +11,6 @@ npm i --no-audit
 
 npx type-predicate-generator --help
 
-rm -rf types_guards.ts types_guards.test.ts
-npx type-predicate-generator --unitTests src/types.ts
-npx esbuild --bundle --outfile=src/types_guards.min.js --minify "src/types_guards.ts"
-
-rm -rf example_guards.ts example_guards.test.ts
-npx type-predicate-generator --unitTests src/example.ts
-npx esbuild --bundle --outfile=src/example_guards.min.js --minify "src/example_guards.ts"
-
 rm src/unsupported1.stdout.txt src/unsupported1.stderr.txt
 npx type-predicate-generator ./src/unsupported1.ts 1>src/unsupported1.stdout.txt 2>src/unsupported1.stderr.txt || true
 
