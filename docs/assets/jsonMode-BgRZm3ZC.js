0c615f1a800ead68f2e6d84deb8ef28c26a2ebb6
Peter Leonov <gojpeg@gmail.com>
2025-01-28 00:21:36 +0100

    update playground

--- docs/assets/jsonMode-BgRZm3ZC.js
+++ docs/assets/jsonMode-BgRZm3ZC.js
@@ -1,4 +1,4 @@
-import{m as Et}from"./index-5v8WT6XG.js";/*!-----------------------------------------------------------------------------
+import{m as Et}from"./index-DqEjqn9w.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
